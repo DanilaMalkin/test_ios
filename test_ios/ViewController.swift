c9b12282e5651ba612a04167fbf978acc411b62d
Danila <danil_malkin@list.ru>
2023-10-09 19:00:30 +0300

    add all

---
+++ test_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_ios
+//
+//  Created by Danila on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
