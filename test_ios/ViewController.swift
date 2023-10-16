7519b0781112caa5824ba6c998766d1da4dca958
Danila <danil_malkin@list.ru>
2023-10-16 19:42:19 +0300

    added url session

--- test_ios/ViewController.swift
+++ test_ios/ViewController.swift
@@ -12,7 +12,25 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        // Do any additional setup after loading the view.
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard
+                let data,
+                let response,
+                error == nil
+            else{
+                return
+            }
+            //let decoder = JSONDecoder()
+            //let jsonModel = decoder.
+            let str = String(data: data, encoding: .utf8)
+            print("Полученные данные: \(str ?? "")")
+        }).resume()
+        
+        
+        
+        
+        
     }
 
 
