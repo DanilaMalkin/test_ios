032c9a3664647b407f6e6222be5cd0c048e47b46
Danila <danil_malkin@list.ru>
2023-11-03 18:19:50 +0300

    add table mainbeer

--- test_ios/ViewController.swift
+++ test_ios/ViewController.swift
@@ -1,46 +1,87 @@
 //
 //  ViewController.swift
-//  test_ios
+//  ios_practical
 //
-//  Created by Danila on 02.10.2023.
+//  Created by AlexGod on 02.10.2023.
 //
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        beerData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let beer = beerData[indexPath.row]
+        var cell = UITableViewCell()
+        var configuration = cell.defaultContentConfiguration()
+        configuration.image = UIImage()
+        configuration.text = beer.name
+        configuration.secondaryText = beer.tagline
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemPink
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    private var beerData: [BeerDTO] = []
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemMint
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
         let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
         URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             guard
                 let data,
-                let response,
                 error == nil
-            else{
-                return
-            }
+            else { return }
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-            //let jsonModel = decoder.
-            let model = try! decoder.decode([BeerDTO].self, from: data)
-            print(model)
-            
+            self.beerData = try! decoder.decode([BeerDTO].self, from: data)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }).resume()
-    
-        
         
     }
-
-
+    
 }
 
-struct BeerDTO: Decodable{
+struct BeerDTO: Decodable {
     let id: Int
     let name: String
     let tagline: String
     let imageUrl: URL
-    
 }
 
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
